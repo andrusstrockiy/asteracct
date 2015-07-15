f04dbd4c3dc866793298c1fbca358317efbeabc2
Andrey Tkachenko <tkachenko@lanbilling.ru>
2015-07-15 17:50:26 +0300

    Logging was transfered to object oriented

--- confiparse.py
+++ confiparse.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 import ConfigParser
-import logging as lg
 import sys
+import logg
+
+
+log = logg.Log()
+
+
+
 
 # Todo make file opener handler and log the message for No option 'radius_server' in section: 'globals'
 
@@ -11,99 +17,66 @@ extensionsconf = '/etc/asterisk/extensions.conf'
 managersconf = '/etc/asterisk/manager.conf'
 asterisk_amihost = "192.168.55.254"
 
-logger = lg.getLogger('configparser')
-logger.setLevel(lg.INFO)
-
-fh = lg.FileHandler("astradclient.log")
-
-formatter = lg.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-fh.setFormatter(formatter)
-logger.addHandler(fh)
 
 confdir = [extensionsconf, managersconf]
-
-# radius_addr = config.get('globals', 'RADIUS_Server')
-# radius_secret = config.get('globals', 'RADIUS_Secret')
-# radius_acct_port = config.get('globals','RAIUS_Acct_Port')
-# rnas_addr = config.get('globals','NAS_IP_Address')
-# asterisk_amihost = "192.168.55.254"
-
-# asterisk_amiport = '5038'
-# asterisk_amiusername = 'test'
-# asterisk_amisecret = 'test'
 
 
 class ConfigOpener():
     """Class which parse configs"""
     global confdir, asterisk_amihost
 
-    def __init__(self, afconfigs=confdir):
-        #
+    def __init__(self):
+        """Initializing the following attributes"""
         self.config = ConfigParser.ConfigParser()
-        #
-        # self.configfiles = self.config.read(afconfigs)
-        #
-        # self.radius_addr = self.config.get('globals', 'RADIUS_Server')
-        #
-        # self.radius_acct_port = self.config.get('globals', 'RAIUS_Acct_Port')
-        # self.rnas_addr = self.config.get('globals', 'NAS_IP_Address')
-        #
-        # logger.info(' -- Radius client settings are Radius address %s and Radius Accounting Ports %s'
-        # % (self.radius_addr, self.radius_acct_port))
-        # self.radius_secret = self.config.get('globals', 'RADIUS_Secret')
-        #
-        #
-        # self.amiport = self.config.get('general', 'port')
-        # self.amienables = self.config.get('general', 'enabled')
-        #
-        # self.amihost = asterisk_amihost
-        # logger.info("Reading AMI manager config")
-        # for managers in ['test', 'lbamimanager']:
-        #     if managers in self.config.sections():
-        #         # logger.setLevel(lg.DEBUG)
-        #         logger.debug("-m- %s" % managers)
-        #         self.amimanager = managers
-        #         self.amisecret = self.config.get(managers, 'secret')
-        #         logger.info("AMI manager login credentials:%s " % managers)
-        #         break
-        #     else:
-        #         logger.critical('AMI manager couldn\'t be found')
-        #         logger.critical('Please set one in /etc/asterisk/manager.conf')
-        #         sys.exit(1)
+        self.configfiles = None
+        self.radius_addr = None
+        self.radius_secret = ""
+        self.radius_acct_port = None
+        self.rnas_addr = None
+        self.raddict = {}
+        # Ami Manager settings:
+        self.amiport = None
+        self.amienables = ''
+        self.amihost = None
+        self.amimanager = ''
+        self.amisecret = ''
+        self.amimdict = {}
+
+
 
     def radius_config(self, afconfigs=extensionsconf):
         self.configfiles = self.config.readfp(open(afconfigs))
-        logger.info(" -- Start reading config settings from file %s " % afconfigs)
+        log.logger.info(" -- Start reading config settings from file %s " % afconfigs)
 
         self.radius_addr = self.config.get('globals', 'RADIUS_Server')
         self.radius_acct_port = self.config.get('globals', 'RAIUS_Acct_Port')
         self.rnas_addr = self.config.get('globals', 'NAS_IP_Address')
-        logger.info(' -- Radius client settings are Radius address %s and Radius Accounting Ports %s'
-                    % (self.radius_addr, self.radius_acct_port))
+        log.logger.info(' -- Radius client settings are Radius address %s and Radius Accounting Ports %s'
+                        % (self.radius_addr, self.radius_acct_port))
         self.radius_secret = self.config.get('globals', 'RADIUS_Secret')
-        logger.info(' -- NAS IP address %s ' % self.radius_addr)
+        log.logger.info(' -- NAS IP address %s ' % self.radius_addr)
         self.raddict = {'radius_addr': self.radius_addr, 'radius_acct_port': self.radius_acct_port,
                         'rnas_addr': self.rnas_addr, 'radius_secret': self.radius_secret}
         return self.raddict
 
     def ami_config(self, afconfig=managersconf):
-        logger.info(" -- Start reading config settings from file %s " % afconfig)
+        log.logger.info(" -- Start reading config settings from file %s " % afconfig)
         self.configfiles = self.config.readfp(open(afconfig))
         self.amiport = self.config.get('general', 'port')
         self.amienables = self.config.get('general', 'enabled')
         self.amihost = asterisk_amihost
-        logger.info("Reading AMI manager config")
+        log.logger.info("Reading AMI manager config")
         for managers in ['test', 'lbamimanager']:
             if managers in self.config.sections():
                 # logger.setLevel(lg.DEBUG)
-                logger.debug("-m- %s" % managers)
+                log.logger.debug("-m- %s" % managers)
                 self.amimanager = managers
                 self.amisecret = self.config.get(managers, 'secret')
-                logger.info("AMI manager login credentials:%s " % managers)
+                log.logger.info("AMI manager login credentials:%s " % managers)
                 break
             else:
-                logger.critical('AMI manager couldn\'t be found')
-                logger.critical('Please set one in /etc/asterisk/manager.conf')
+                log.logger.critical('AMI manager couldn\'t be found')
+                log.logger.critical('Please set one in /etc/asterisk/manager.conf')
                 sys.exit(1)
         self.amimdict = {'ami_port': self.amiport, 'ami_enabled': self.amienables, 'ami_host': self.amihost,
                          'ami_manager': self.amimanager, 'ami_secret': self.amisecret}
--- logg.py
+++ logg.py
@@ -3,13 +3,13 @@
 import logging as lg
 
 
-class log():
+class Log():
     def __init__(self):
         self.logger = lg.getLogger(__name__)
-        self.logger.setLevel(lg.INFO)
+
         # create a file handler
-        self.handler = lg.FileHandler('hello.log')
-        self.handler.setLevel(lg.INFO)
+        self.handler = lg.FileHandler('astradclient.log')
+
         # create a logging format
         self.formatter = lg.Formatter('%(asctime)s  %(name)s -%(thread)s %(threadName)s '
                                       ' %(module)s- [%(levelname)s] - %(message)s')
@@ -17,11 +17,3 @@ class log():
         # add the handlers to the logger
         self.logger.addHandler(self.handler)
 
-
-a = log()
-a.logger.info("Starting to log")
-a.logger.debug('1111')
-a.logger.setLevel(lg.DEBUG)
-a.logger.debug('Debug leve -  level to debug')
-a.logger.setLevel(lg.INFO)
-a.logger.info("back to info")
