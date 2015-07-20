582751eb1cce6430e06357ec0dd8a0c6b03359a4
Andrey Tkachenko <tkachenko@lanbilling.ru>
2015-07-20 16:33:44 +0300

    introduced setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='Asterisk Accouting',
+    version='0.0.1',
+    packages=['pyrad-2.0.pyrad', 'pyrad-2.0.pyrad.tests', 'pyst-0.6.50.build.lib.linux-i686-2.7.asterisk',
+              'pyst-0.6.50.asterisk'],
+    url='',
+    license='GPL',
+    author='Andrew Tkachenko',
+    author_email='trockiy@gmail.com',
+    description='Asterisk Radius Accounting script'
+)
